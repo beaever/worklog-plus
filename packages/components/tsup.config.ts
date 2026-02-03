64674f8e6d3bca38be0738134213df9495a78ba2
beaever <thebeaever@gmail.com>
2026-02-03 16:48:30 +0900

    [feature]tsup-setting - packages>ui,components tsup install - tsup 을 활용하여 경량화 작업

---
+++ packages/components/tsup.config.ts
@@ -0,0 +1,13 @@
+import { defineConfig } from 'tsup';
+
+export default defineConfig({
+  entry: ['src/index.ts'],
+  format: ['cjs', 'esm'],
+  dts: true,
+  splitting: true,
+  sourcemap: true,
+  clean: true,
+  external: ['react', 'react-dom', '@worklog/ui', '@worklog/types'],
+  treeshake: true,
+  minify: true,
+});
